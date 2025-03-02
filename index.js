a335733c16d33168769745f899b2766e1ac9419f
Marisel <shenapam3@gmail.com>
2025-03-02 21:07:03 +0300

    Update index.js
    Signed-off-by: Marisel <shenapam3@gmail.com>

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+
+
+
+
 // coded by 💗
 import dotenv from 'dotenv';
 dotenv.config();
@@ -96,21 +100,8 @@ async function start() {
                 }
             } else if (connection === 'open') {
                 if (initialConnection) {
-                    console.log(chalk.green("DEMON-SLAYER-XMD IS CONNECTED SUCCESSFULLY  "));
-            Matrix.sendMessage(Matrix.user.id, { 
-                image: { url: "https://files.catbox.moe/76x3kn.jpg" }, 
-                caption: `*Hello there DEMON-SLAYER-XMD User! 👋🏻* 
-
-Simple, Straightforward, But Loaded With Features 🎊. Meet DEMON-SLAYER-XMD WhatsApp Bot.
-REPORT ANY PROBLEM TO THE FOLLOWING MAIN DEVS 
- *MAIN DEVS*
-© BRUCE BERA
-© JOEL JAMES  
-© MARISEL 
-                   
-    
-> © MADE BY  CREW SLAYER`
-            });
+                    console.log(chalk.green("αм ʝσєℓ χ∂ ν тяєє вσт"));
+                    Matrix.sendMessage(Matrix.user.id, { text: `αм ʝσєℓ χ∂ ν тняєє вσт` });
                     initialConnection = false;
                 } else {
                     console.log(chalk.blue("♻️ Connection reestablished after restart."));
@@ -178,4 +169,4 @@ app.listen(PORT, () => {
 });
 
 
-//updated by lord joel
+//updated by lord joel 
